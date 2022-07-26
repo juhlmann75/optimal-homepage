f14134397d5f617c79cea4b5b63569c8c09ff458
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-07-25 21:40:20 -0500

    add current date and time

---
+++ components/currentDate.tsx
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+
+export default function CurrentDate() {
+    const locale = 'en';
+    const [today, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+
+    const day = today.toLocaleDateString(locale, { weekday: 'long' });
+    const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
+
+    const hour = today.getHours();
+    const wish = `Good ${(hour < 12 && 'Morning') || (hour < 17 && 'Afternoon') || 'Evening'}, `;
+
+    const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
+
+    return (
+        <div className="flex items-center justify-center m-4">
+            {date} {time}
+        </div>
+    );
+}
