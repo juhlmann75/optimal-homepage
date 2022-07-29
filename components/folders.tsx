03824303effc2c397b2455671db73fa9cf080eea
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-07-28 23:35:26 -0500

    increase size of darkmode icon

--- components/folders.tsx
+++ components/folders.tsx
@@ -2,9 +2,6 @@ import {useLiveQuery} from "dexie-react-hooks";
 import {db} from "../models/db";
 import Bookmarks from "./bookmarks";
 import BookmarksUpload from "./bookmarksUpload";
-import ToggleButton from "./toggleButton";
-import BookmarksUploadModal from "./bookmarksUploadModal";
-import CurrentDate from "./currentDate";
 
 export default function Folders() {
 
@@ -21,19 +18,14 @@ export default function Folders() {
     }
 
     return (
-        <div>
-            <div className="flex items-center justify-center m-4">
-                <CurrentDate></CurrentDate>
-            </div>
-            <div className="flex flex-wrap justify-center">
-                {folders.map(folder => (
-                    <div className="border-solid border-2 border-sky-500 rounded p-2 m-3 w-80" key={folder.id}>
-                        <h3 className="text-2xl font-bold mb-2">{folder.name}</h3>
-                        <hr />
-                        <Bookmarks folderId={folder.id}></Bookmarks>
-                    </div>
-                ))}
-            </div>
+        <div className="flex flex-wrap justify-center">
+            {folders.map(folder => (
+                <div className="border-solid border-2 border-sky-500 rounded p-2 m-3 w-80" key={folder.id}>
+                    <h3 className="text-2xl font-bold mb-2">{folder.name}</h3>
+                    <hr/>
+                    <Bookmarks folderId={folder.id}></Bookmarks>
+                </div>
+            ))}
         </div>
     )
 }
