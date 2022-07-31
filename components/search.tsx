364b813252bc3c89e15880f1637bf5d118b2f570
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-07-31 18:01:04 -0500

    useEffect google script

--- components/search.tsx
+++ components/search.tsx
@@ -1,11 +1,33 @@
 import Script from "next/script";
+import {useEffect, useState} from "react";
 
 export default function Search() {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        const scriptTag = document.createElement('script');
+        scriptTag.src = 'https://cse.google.com/cse.js?cx=f175aeb8db7f24a31';
+        scriptTag.addEventListener('load', ()=>setLoaded(true));
+        document.body.appendChild(scriptTag);
+    }, []);
+
+    useEffect(() => {
+        if (!loaded) return;
+        const searchBox = document.getElementById("gsc-i-id1");
+
+        if (searchBox != null) {
+            console.log("not null");
+            searchBox.focus();
+        }
+        console.log(document);
+        console.log(loaded);
+    }, [loaded]);
+
     return (
-        <div className="flex flex-col mx-20 my-4 text-black search">
-            <Script async src="https://cse.google.com/cse.js?cx=f175aeb8db7f24a31">
-            </Script>
-            <div className="gcse-search"></div>
+        <div className="text-center search">
+            <div className="max-w-lg mx-auto my-3">
+                <div className="gcse-search"></div>
+            </div>
         </div>
     )
 }
