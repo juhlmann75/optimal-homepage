49998d1dbb5ba6ac8fced2f1ae61acb8c12746f1
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-08-07 17:40:24 -0500

    enable ssr in firebase

--- components/quote.tsx
+++ components/quote.tsx
@@ -1,36 +1,18 @@
-import {useEffect, useState} from "react";
-
 export interface QuoteData {
     q: string,
     a: string,
     h: string
 }
 
-export default function Quote() {
-    const quoteApiUrl = '/api/today';
-    const [quote, setQuote] = useState<QuoteData>()
-    const [isLoading, setLoading] = useState(false)
-
-    useEffect(() => {
-        setLoading(true)
-        fetch(quoteApiUrl)
-            .then((res) => res.json())
-            .then((data: QuoteData[]) => {
-                setQuote(data[0])
-                setLoading(false)
-            })
-    }, [])
-
-    if (isLoading) return <p>Loading...</p>
-    if (!quote) return <p>No data</p>
+export default function Quote({data}: {data: QuoteData[]}) {
 
     return (
         <div>
             <blockquote>
-                <p className="text-center text-3xl subpixel-antialiased mt-8">&ldquo;{quote.q}&rdquo;</p>
+                <p className="text-center text-3xl subpixel-antialiased mt-8">&ldquo;{data[0].q}&rdquo;</p>
             </blockquote>
             <figcaption className="flex items-center justify-center my-3 space-x-3">
-                <div className="pr-3 font-medium text-gray-900 dark:text-white">- {quote.a}</div>
+                <div className="pr-3 font-medium text-gray-900 dark:text-white">- {data[0].a}</div>
             </figcaption>
         </div>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import Folders from "../components/folders";
 import TopNavbar from "../components/topNavbar";
 import CurrentDate from "../components/currentDate";
 import Search from "../components/search";
-import Quote from "../components/quote";
+import Quote, {QuoteData} from "../components/quote";
 
-export default function Home() {
+export default function Home({ data }: {data: QuoteData[]}) {
     return (
         <div>
             <TopNavbar></TopNavbar>
@@ -14,10 +14,19 @@ export default function Home() {
                 <div className="flex items-center justify-center m-4">
                     <CurrentDate></CurrentDate>
                 </div>
-                {/*<Quote></Quote>*/}
+                <Quote data={data}></Quote>
                 <Search></Search>
                 <Folders></Folders>
             </Layout>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`https://zenquotes.io/api/today`)
+    const data = await res.json()
+
+    // Pass data to the page via props
+    return { props: { data } }
 }
